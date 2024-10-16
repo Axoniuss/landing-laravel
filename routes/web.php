9888bb33eab03a456573ed752c561c116eb9b929
klim <klim.vlad@list.ru>
2024-10-16 10:46:17 +0300

    change

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use Database\Factories\UserFactory;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+    //     return view('welcome');
+    // });
+    // Route::view('/', 'welcome');
+    // Route::view('/home', 'welcome');
+    // Route::get('/user', UserController::class);
+    Route::get('posts', [PostController::class, 'index'])->name('posts');
+    Route::get('posts/create', [PostController::class, 'create'])->name('posts.create');
+    Route::post('posts', [PostController::class, 'store'])->name('posts.store');
+    Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
+    Route::get('posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('posts/{post}', [PostController::class, 'update'])->name('posts.update');
+    Route::delete('posts/{post}', [PostController::class, 'delete'])->name('posts.delete');
+    Route::put('posts/{post}/like', [PostController::class, 'like'])->name('posts.like');

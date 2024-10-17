bae3483244e6c816c18f7b0e516b1d980e37992b
klim <klim.vlad@list.ru>
2024-10-17 15:11:06 +0300

    providers and folder allocation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -38,19 +38,19 @@ class PostController extends Controller
         return ('Запрос изменения поста');
     }
 
-    public function show(Post $post)
+    public function show( $post)
     {
         // return view('user.posts.show', compact('post'));
         return ('страница просмотр поста');
     }
 
-    public function edit(Post $post)
+    public function edit( $post)
     {
         // return view('user.posts.edit', compact('post'));
         return ('страница изменения поста');
     }
 
-    public function update(Request $request, Post $post)
+    public function update(Request $request,  $post)
     {
         // $validated = validate($request->all(), Post::$rules);
 
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,25 @@
 <?php
-
 use App\Http\Controllers\PostController;
-use Database\Factories\UserFactory;
+use App\Http\Controllers\Posts\CommentController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-    //     return view('welcome');
-    // });
-    // Route::view('/', 'welcome');
-    // Route::view('/home', 'welcome');
-    // Route::get('/user', UserController::class);
-    Route::get('posts', [PostController::class, 'index'])->name('posts');
-    Route::get('posts/create', [PostController::class, 'create'])->name('posts.create');
-    Route::post('posts', [PostController::class, 'store'])->name('posts.store');
-    Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
-    Route::get('posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
-    Route::put('posts/{post}', [PostController::class, 'update'])->name('posts.update');
-    Route::delete('posts/{post}', [PostController::class, 'delete'])->name('posts.delete');
-    Route::put('posts/{post}/like', [PostController::class, 'like'])->name('posts.like');
+// Route::view('/', 'welcome');
+
+// Маршрут для отображения страницы /home
+// Route::view('/home', 'welcome');
+
+// Маршрут для лайка поста, которого нет в стандартных RESTful маршрутах
+// Route::prefix('user')->middleware('auth', 'active')->group(function () {
+Route::prefix('user')->group(function () {
+    Route::redirect('/', '/user/posts')->name('user');
+
+    Route::get('posts', [PostController::class, 'index'])->name('user.posts');
+    Route::get('posts/create', [PostController::class, 'create'])->name('user.posts.create');
+    Route::post('posts', [PostController::class, 'store'])->name('user.posts.store');
+    Route::get('posts/{post}', [PostController::class, 'show'])->name('user.posts.show')->whereNumber('post');
+    Route::get('posts/{post}/edit', [PostController::class, 'edit'])->name('user.posts.edit')->whereNumber('post');
+    Route::put('posts/{post}', [PostController::class, 'update'])->name('user.posts.update')->whereNumber('post');
+    Route::delete('posts/{post}', [PostController::class, 'delete'])->name('user.posts.delete')->whereNumber('post');
+//40 создаl providers и нужно в роутсах распределить папки
+    // Route::get('donates', DonateController::class)->name('user.donates');
+});
